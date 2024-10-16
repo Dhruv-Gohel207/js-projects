2c1edda66d293f97e8ddafe11b6af19eb68a6914
Dhruv-Gohel207 <goheldhruv7473@gmail.com>
2024-10-16 09:39:02 +0000

    datatypes

---
+++ 1_basics/datatype.js
@@ -0,0 +1,16 @@
+let name = "dhruv"    //string ""
+let age = 20          //number 2 power 53 
+let isLoggedIn = true //boolean  true/false
+
+
+
+//others 
+//null => standalone value
+//undefine =>
+//symbol 
+//object
+
+
+console.log(typeof undefined); //undefined
+console.log(typeof null);   //object
+
---
+++ 1_basics/vartry.js
@@ -0,0 +1,14 @@
+const accountId  = 1234
+let accountEmail = "dhruv207@email.com"
+var accountPassword = "13541"
+ accountCity = "surat"
+
+
+// accountId = 4152
+// console.log(accountId);
+
+accountEmail = "goheld854@mail.com"
+accountCity = "palitana"
+
+console.table([accountId, accountEmail, accountPassword, accountCity]);
+
