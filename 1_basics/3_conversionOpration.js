5fb7d2e45f7c0f2b78b75aec998cacc8b74698e7
Dhruv-Gohel207 <goheldhruv7473@gmail.com>
2024-10-16 12:36:52 +0000

    operation

--- 1_basics/3_conversionOpration.js
+++ 1_basics/3_conversionOpration.js
@@ -28,7 +28,37 @@ let find  =true
 */
 let someNumb = 33
 
-let stringNumb = String(someNumb)
-console.log(stringNumb);
-console.log(typeof stringNumb);
+// let stringNumb = String(someNumb)
+// console.log(stringNumb);
+// console.log(typeof stringNumb);
+
+
+
+/* --------------------------------------------------oprations----------------------------------------------------------*/
+
+
+let value = 3
+let negValue = -value
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*4);
+// console.log(2/2);
+// console.log(2**5);
+// console.log(2%5);
+
+// let str1 = "new"
+// let str2 = " text"
+
+// let str3 = str1 + str2
+
+// console.log(str3);
+
+console.log("1" + 2);
+console.log(1 + "2");
+console.log("1" + 2 + 2);
+console.log(1 + 2 + "2");
+console.log(1 + "2" + 2);
+
 
