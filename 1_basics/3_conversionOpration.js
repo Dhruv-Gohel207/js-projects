5c65f29fa257bcb1b6b6e7f80255f832511c3f04
Dhruv-Gohel207 <goheldhruv7473@gmail.com>
2024-10-16 12:03:37 +0000

    conversion

---
+++ 1_basics/3_conversionOpration.js
@@ -0,0 +1,34 @@
+let find  =true
+
+// console.log(typeof find);
+// console.log(typeof (find));
+
+// let valueInNumber = Number(find)
+// console.log(typeof valueInNumber);
+
+
+// console.log(valueInNumber);
+
+
+//summary 
+/*
+    "33" => 33
+    "33abc" => NaN
+    true => 1 flase=>0
+*/
+
+// let isLoggedIn = 0
+
+// let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
+
+/*in boolean
+    1=> true 0=> false
+    ""=>false  "nafaui"=>true
+*/
+let someNumb = 33
+
+let stringNumb = String(someNumb)
+console.log(stringNumb);
+console.log(typeof stringNumb);
+
