36dbdaebe0805a1f183dd9235a0efd78cca8487e
Dhruv-Gohel207 <goheldhruv7473@gmail.com>
2024-10-16 10:12:50 +0000

    datatypes

--- 1_basics/datatype.js
+++ 1_basics/datatype.js
@@ -11,6 +11,9 @@ let isLoggedIn = true //boolean  true/false
 //object
 
 
-console.log(typeof undefined); //undefined
-console.log(typeof null);   //object
+// console.log(typeof undefined); //undefined
+// console.log(typeof null);   //object
+
+console.log(typeof age);
+console.log(typeof isLoggedIn);
 
