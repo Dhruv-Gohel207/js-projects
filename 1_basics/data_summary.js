e9f01c244b7614c8d8ad329b5a27c1e80a39ca6b
Dhruv-Gohel207 <goheldhruv7473@gmail.com>
2024-10-18 04:22:23 +0000

    summary

---
+++ 1_basics/data_summary.js
@@ -0,0 +1,24 @@
+//Stak (Primitive) Heap (Non - Primitive) 
+
+
+    let myCarName = "porsche"
+
+    let anotherName = myCarName 
+    anotherName = "BMW"
+
+    console.log(myCarName);
+    console.log(anotherName);
+
+
+    let firstUser = {
+        email : "user@something.com",
+        upi: "user@bbl"
+    }
+    
+    let userTwo = firstUser
+
+    userTwo.email = "har@something.com"
+
+    console.log(firstUser.email);
+    console.log(userTwo.email);
+    
