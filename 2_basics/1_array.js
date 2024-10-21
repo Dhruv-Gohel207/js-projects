018f30ff6e96ed6f0e488237f918025ab7608f71
Dhruv-Gohel207 <goheldhruv7473@gmail.com>
2024-10-21 12:36:49 +0000

    first array

---
+++ 2_basics/1_array.js
@@ -0,0 +1,38 @@
+
+const myArr = [0, 1, 2, 3, 4, 5 ]
+const myCars =["porshe", "volvo", "bmw", "mclearn"]
+
+const arr2 = new Array (1, 2, 3, 4, 5)
+
+// console.log(arr2[1]);    
+
+//Araay method
+
+// myArr.push(6)
+// myArr.pop(6)
+
+// myArr.unshift(9)
+myArr.shift()   
+
+// console.log(myArr.includes(2))
+// console.log(myArr.indexOf(8))
+
+// const newarr = myArr.join()
+// console.log(myArr)
+// console.log(typeof newarr)
+
+//slice and splice
+
+// console.log("A ", myArr);
+// const myn1 = myArr.slice(1, 3)
+
+// console.log(myn1);
+// console.log("B ", myArr);
+
+// const myn2 = myArr.splice(1, 3)
+// console.log("C ", myArr);
+// console.log(myn2);
+
+
+
+
