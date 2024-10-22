b9e608d1c19ffafbb6cf7bd17df8d2170d6491e0
Dhruv-Gohel207 <goheldhruv7473@gmail.com>
2024-10-22 12:41:43 +0000

    fun

---
+++ 3_basics/01_function.js
@@ -0,0 +1,16 @@
+function myName  (){
+    console.log("D");
+    console.log("h");
+    console.log("r");
+    console.log("u");
+    console.log("v");
+}
+
+// myName()
+
+function addition (num1, num2){
+
+    console.log(num1+num2);
+    
+}
+addition(20,50)
