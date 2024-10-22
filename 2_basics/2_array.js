6caca3e0b516af13049f2aab43ed07753aa9d764
Dhruv-Gohel207 <goheldhruv7473@gmail.com>
2024-10-22 05:54:33 +0000

    array2 done

--- 2_basics/2_array.js
+++ 2_basics/2_array.js
@@ -2,4 +2,26 @@ const Marvel_heros = ["iron-man", "thor", "hulk"]
 const Dc_heros = ["batman", "superman", "flash"]    
 
 
-console.log(Marvel_heros.concat(Dc_heros))
+// console.log(Marvel_heros.concat(Dc_heros))
+const newheros = [...Marvel_heros, ...Dc_heros]
+// console.log(newheros);
+
+
+const nes_array = [1, 2, 3, [4, 5, 6], 7, 8, [9, 10, [1, 2]]]
+    // console.log(nes_array.flat(Infinity));
+
+
+    console.log(Array.isArray("abcd"));
+    console.log(Array.from("abcd"));
+    console.log(Array.of("abcd"));
+    console.log(Array.from({name:"abcd"}));  //its given an empty array
+
+let num1 = 100
+let num2 = 200
+let num3 = 300
+
+console.log(Array.of(num1, num2, num3));
+
+
+    
+    
