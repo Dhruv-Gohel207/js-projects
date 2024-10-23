b650687f8d7f329e3dca48e5fdf28198e68c423d
Dhruv-Gohel207 <goheldhruv7473@gmail.com>
2024-10-23 07:03:18 +0000

    functons

--- 3_basics/01_function.js
+++ 3_basics/01_function.js
@@ -1,16 +1,57 @@
-function myName  (){
-    console.log("D");
-    console.log("h");
-    console.log("r");
-    console.log("u");
-    console.log("v");
-}
+// function myName  (){
+//     console.log("D");
+//     console.log("h");
+//     console.log("r");
+//     console.log("u");
+//     console.log("v");
+// }
 
-// myName()
+// // myName()
 
-function addition (num1, num2){
+// function addition (num1, num2){
 
-    console.log(num1+num2);
+//     // let Result =num1+num2
+//     // return Result
+
+//     // return num1+num2
     
+// }
+//  const Result = addition(20,50)
+// // console.log("result :", Result);
+
+
+// // function logMsg(username){
+//     // if(!username){
+//         // console.log("please enter the username");
+//         // return
+//     // }
+//     // return `${username} just logged in`
+// // }
+// // console.log(logMsg());
+
+
+// function count  (val1, ...num1){
+//     // return num1
+// }
+// // console.log(count(200,400,600));
+
+
+const user = {
+    // username: "Dhruv",
+    id:20
 }
-addition(20,50)
+
+function handleObj(anyobj){
+    // console.log(`username is ${anyobj.username} and id is ${anyobj.id}`);
+     
+}
+// handleObj(user) 
+
+
+const newArry = [200, 400, 800, 2000]
+
+function returnvalues(getArray){
+    return getArray[1]
+}
+console.log(returnvalues(newArry));
+
