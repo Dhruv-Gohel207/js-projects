31f6005169b66035b9b9013c19c9cd2f53990bf5
Dhruv-Gohel207 <goheldhruv7473@gmail.com>
2024-10-24 05:34:23 +0000

    scopes done

--- 3_basics/02_scope.js
+++ 3_basics/02_scope.js
@@ -3,9 +3,50 @@ let a = 300
 if(true){
     let a = 10
     const b = 20
-    console.log("Inner :", a);
+    // console.log("Inner :", a);
 }
 
-console.log(a);
+// console.log(a);
 // console.log(b);
 // console.log(c);
+
+
+// function parenet (){
+//     const userone = "dharm"
+
+//     function child(){
+//         const usertwo = "satya"
+//         // console.log(userone);   
+
+        
+//     }
+//     child()
+//     // console.log(usertwo);
+
+// }
+// parenet()
+
+
+
+if (true) {
+    const aa= "abc"
+    if (aa==="abc") {
+        const bb = " pqr"
+        // console.log(aa + bb);
+    }
+    // console.log(bb);
+    
+}
+// console.log(aa);
+
+//++++++++++++++++++++++++++++++++++++ New Concept ++++++++++++++++++++++++++++++++++++
+
+console.log( AddValue(5)); 
+function AddValue(num){   //this function can access thw value anywhere
+    return  num +1
+}
+
+Addvalue2(5)
+const Addvalue2= function (num1){ //if we same that like upper function its not working 
+    return num1+2
+}
