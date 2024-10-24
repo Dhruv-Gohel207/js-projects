331dd933f51d0cbbd3ad1e197037770c175e1117
Dhruv-Gohel207 <goheldhruv7473@gmail.com>
2024-10-24 09:24:04 +0000

    iife done

---
+++ 3_basics/04_iife.js
@@ -0,0 +1,13 @@
+// Immediately Invoked Function Expression (IIFE)
+
+
+(function txt(){
+    //named iife 
+    console.log('anything');
+    
+})(); // there is semicolon needed if you dont put it invoke i not stopped and give error in othee funtion]
+
+( (name)=>{
+    console.log(`new txt ${name}`);
+    
+} )("prince"); 
