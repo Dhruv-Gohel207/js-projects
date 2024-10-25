2968ddbc7b94ab7affd4642926e80f9055cad49b
Dhruv-Gohel207 <goheldhruv7473@gmail.com>
2024-10-25 17:47:23 +0530

    do while done

---
+++ 5_itration_loops/dowhileloop.js
@@ -0,0 +1,24 @@
+
+// let i = 0
+// while(i <= 10){
+//     console.log(`Value of index is : ${i}`);
+//     i = i + 2
+// }
+
+
+let myArr = ['batman', 'superman', 'flash']
+
+let arr = 0
+
+while (arr < myArr.length){
+        console.log(`value is : ${myArr[arr]}`);
+        arr++
+}
+
+
+let point = 1
+ do {
+    console.log(`point is ${point}`);
+    point++
+    
+ } while (point <= 10);
